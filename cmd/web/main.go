c094c2d0a88557f19382154ee511d467b59b0547
Jimmy Prescott <bunches_stands_0s@icloud.com>
2023-12-04 08:20:05 -0600

    cuddles

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,11 +7,9 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	mux.HandleFunc("/", home)
 
 	log.Print("starting server on :4000")
